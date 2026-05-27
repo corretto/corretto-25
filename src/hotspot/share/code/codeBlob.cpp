090744378b9fa20cfc48517660768f9fe05c8b89
Evgeny Astigeevich <eastig@amazon.com>
2026-05-27 14:03:06 +0000

    8380710: nmethod::finalize_relocations() should be called before ICache::invalidate_range() is called
    Reviewed-by: adinn, dlong
    
    Backport-of: 79668b007db71921938d7639ba15f8ce44682f4f

--- src/hotspot/share/code/codeBlob.cpp
+++ src/hotspot/share/code/codeBlob.cpp
@@ -38,6 +38,7 @@
 #include "prims/forte.hpp"
 #include "prims/jvmtiExport.hpp"
 #include "runtime/handles.inline.hpp"
+#include "runtime/icache.hpp"
 #include "runtime/interfaceSupport.inline.hpp"
 #include "runtime/javaFrameAnchor.hpp"
 #include "runtime/jniHandles.inline.hpp"
@@ -338,6 +339,9 @@ RuntimeBlob::RuntimeBlob(
   }
 
   cb->copy_code_and_locs_to(this);
+
+  // Flush generated code
+  ICache::invalidate_range(code_begin(), code_size());
 }
 
 void RuntimeBlob::free(RuntimeBlob* blob) {
