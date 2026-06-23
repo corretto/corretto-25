adoptium/jdk

8326205: Grouping frequently called C2 nmethods in CodeCache
Co-authored-by: Evgeny Astigeevich <eastigeevich@openjdk.org>
Reviewed-by: kvn, cslucas, aph

src/hotspot/share/runtime/globals.hpp
@@ -1514,6 +1514,10 @@ const int ObjectAlignmentInBytes = 8;
           "Size of code heap with non-nmethods (in bytes)")                 \
           constraint(VMPageSizeConstraintFunc, AtParse)                     \
                                                                             \
+  product(size_t, HotCodeHeapSize, 0, EXPERIMENTAL,                         \
+          "Size of code heap with predicted hot methods (in bytes)")        \
+          range(0, SIZE_MAX)                                                \
+                                                                            \
   product_pd(size_t, CodeCacheExpansionSize,                                \
           "Code cache expansion size (in bytes)")                           \
           range(32*K, SIZE_MAX)                                             \

src/hotspot/share/runtime/hotCodeSampler.hpp
@@ -1,0 +1,104 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifdef COMPILER2
+#ifndef SHARE_RUNTIME_HOTCODESAMPLER_HPP
+#define SHARE_RUNTIME_HOTCODESAMPLER_HPP
+
+#include "runtime/javaThread.hpp"
+#include "runtime/suspendedThreadTask.hpp"
+#include "runtime/threadSMR.hpp"
+#include "utilities/pair.hpp"
+#include "utilities/resizableHashTable.hpp"
+
+// Generate a random sampling period between min and max
+static inline uint rand_sampling_period_ms() {
+  assert(HotCodeMaxSamplingMs >= HotCodeMinSamplingMs, "max cannot be smaller than min");
+  julong range = (julong)HotCodeMaxSamplingMs - (julong)HotCodeMinSamplingMs + 1;
+  return (uint)(os::random() % range) + HotCodeMinSamplingMs;
+}
+
+class ThreadSampler;
+
+class Candidates : public StackObj {
+ private:
+  GrowableArray<Pair<nmethod*, int>> _candidates;
+  int _hot_sample_count;
+  int _non_profiled_sample_count;
+
+ public:
+  Candidates(ThreadSampler& sampler);
+
+  void add_candidate(nmethod* nm, int count);
+  void add_hot_sample_count(int count);
+  void add_non_profiled_sample_count(int count);
+  void sort();
+
+  bool has_candidates();
+  nmethod* get_candidate();
+  double get_hot_sample_percent();
+};
+
+class GetPCTask : public SuspendedThreadTask {
+ private:
+  address _pc;
+
+  void do_task(const SuspendedThreadTaskContext& context) override {
+    JavaThread* jt = JavaThread::cast(context.thread());
+    if (jt->thread_state() != _thread_in_native && jt->thread_state() != _thread_in_Java) {
+      return;
+    }
+    _pc = os::fetch_frame_from_context(context.ucontext(), nullptr, nullptr);
+  }
+
+ public:
+  GetPCTask(JavaThread* thread) : SuspendedThreadTask(thread), _pc(nullptr) {}
+
+  address pc() const {
+    return _pc;
+  }
+};
+
+class ThreadSampler : public StackObj {
+ private:
+  static const int INITIAL_TABLE_SIZE = 109;
+
+  // Table of nmethods found during profiling with sample count
+  ResizeableHashTable<nmethod*, int, AnyObj::C_HEAP, mtInternal> _samples;
+
+ public:
+  ThreadSampler() : _samples(INITIAL_TABLE_SIZE, HotCodeSampleSeconds * 1000 / HotCodeMaxSamplingMs) {}
+
+  // Iterate over and sample all Java threads
+  void sample_all_java_threads();
+
+  // Iterate over all samples with a callback function
+  template<typename Function>
+  void iterate_samples(Function func) {
+    _samples.iterate_all(func);
+  }
+};
+
+#endif // SHARE_RUNTIME_HOTCODESAMPLER_HPP
+#endif // COMPILER2

test/lib/jdk/test/whitebox/code/BlobType.java
@@ -46,8 +46,16 @@ public enum BlobType {
                     || type == BlobType.MethodNonProfiled;
         }
     },
+    MethodHot(2, "CodeHeap 'hot nmethods'", "HotCodeHeapSize") {
+        @Override
+        public boolean allowTypeWhenOverflow(BlobType type) {
+            return super.allowTypeWhenOverflow(type)
+                    || type == BlobType.MethodNonProfiled
+                    || type == BlobType.MethodProfiled;
+        }
+    },
     // Non-nmethods like Buffers, Adapters and Runtime Stubs
-    NonNMethod(2, "CodeHeap 'non-nmethods'", "NonNMethodCodeHeapSize") {
+    NonNMethod(3, "CodeHeap 'non-nmethods'", "NonNMethodCodeHeapSize") {
         @Override
         public boolean allowTypeWhenOverflow(BlobType type) {
             return super.allowTypeWhenOverflow(type)
@@ -56,7 +64,7 @@ public enum BlobType {
         }
     },
     // All types (No code cache segmentation)
-    All(3, "CodeCache", "ReservedCodeCacheSize");
+    All(4, "CodeCache", "ReservedCodeCacheSize");
 
     public final int id;
     public final String sizeOptionName;
@@ -98,6 +106,10 @@ public enum BlobType {
                 || whiteBox.getIntxVMFlag("TieredStopAtLevel") <= 1) {
             // there is no MethodProfiled in non tiered world or pure C1
             result.remove(MethodProfiled);
+        }
+
+        if (Long.valueOf(0).equals(whiteBox.getVMFlag("HotCodeHeapSize"))) {
+            result.remove(MethodHot);
         }
         return result;
     }

corretto/corretto-25

src/hotspot/share/runtime/globals.hpp
@@ -1551,6 +1551,10 @@ const int ObjectAlignmentInBytes = 8;
           "Size of code heap with non-nmethods (in bytes)")                 \
           constraint(VMPageSizeConstraintFunc, AtParse)                     \
                                                                             \
+  product(uintx, HotCodeHeapSize, 0, EXPERIMENTAL,                          \
+          "Size of code heap with predicted hot methods (in bytes)")        \
+          range(0, max_uintx)                                               \
+                                                                            \
   product_pd(uintx, CodeCacheExpansionSize,                                 \
           "Code cache expansion size (in bytes)")                           \
           range(32*K, max_uintx)                                            \

src/hotspot/share/runtime/hotCodeSampler.hpp
@@ -1,0 +1,104 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifdef COMPILER2
+#ifndef SHARE_RUNTIME_HOTCODESAMPLER_HPP
+#define SHARE_RUNTIME_HOTCODESAMPLER_HPP
+
+#include "runtime/javaThread.hpp"
+#include "runtime/suspendedThreadTask.hpp"
+#include "runtime/threadSMR.hpp"
+#include "utilities/pair.hpp"
+#include "utilities/resizeableResourceHash.hpp"
+
+// Generate a random sampling period between min and max
+static inline uint rand_sampling_period_ms() {
+  assert(HotCodeMaxSamplingMs >= HotCodeMinSamplingMs, "max cannot be smaller than min");
+  julong range = (julong)HotCodeMaxSamplingMs - (julong)HotCodeMinSamplingMs + 1;
+  return (uint)(os::random() % range) + HotCodeMinSamplingMs;
+}
+
+class ThreadSampler;
+
+class Candidates : public StackObj {
+ private:
+  GrowableArray<Pair<nmethod*, int>> _candidates;
+  int _hot_sample_count;
+  int _non_profiled_sample_count;
+
+ public:
+  Candidates(ThreadSampler& sampler);
+
+  void add_candidate(nmethod* nm, int count);
+  void add_hot_sample_count(int count);
+  void add_non_profiled_sample_count(int count);
+  void sort();
+
+  bool has_candidates();
+  nmethod* get_candidate();
+  double get_hot_sample_percent();
+};
+
+class GetPCTask : public SuspendedThreadTask {
+ private:
+  address _pc;
+
+  void do_task(const SuspendedThreadTaskContext& context) override {
+    JavaThread* jt = JavaThread::cast(context.thread());
+    if (jt->thread_state() != _thread_in_native && jt->thread_state() != _thread_in_Java) {
+      return;
+    }
+    _pc = os::fetch_frame_from_context(context.ucontext(), nullptr, nullptr);
+  }
+
+ public:
+  GetPCTask(JavaThread* thread) : SuspendedThreadTask(thread), _pc(nullptr) {}
+
+  address pc() const {
+    return _pc;
+  }
+};
+
+class ThreadSampler : public StackObj {
+ private:
+  static const int INITIAL_TABLE_SIZE = 109;
+
+  // Table of nmethods found during profiling with sample count
+  ResizeableResourceHashtable<nmethod*, int, AnyObj::C_HEAP, mtInternal> _samples;
+
+ public:
+  ThreadSampler() : _samples(INITIAL_TABLE_SIZE, HotCodeSampleSeconds * 1000 / HotCodeMaxSamplingMs) {}
+
+  // Iterate over and sample all Java threads
+  void sample_all_java_threads();
+
+  // Iterate over all samples with a callback function
+  template<typename Function>
+  void iterate_samples(Function func) {
+    _samples.iterate_all(func);
+  }
+};
+
+#endif // SHARE_RUNTIME_HOTCODESAMPLER_HPP
+#endif // COMPILER2

test/lib/jdk/test/whitebox/code/BlobType.java
@@ -46,8 +46,16 @@ public enum BlobType {
                     || type == BlobType.MethodNonProfiled;
         }
     },
+    MethodHot(2, "CodeHeap 'hot nmethods'", "HotCodeHeapSize") {
+        @Override
+        public boolean allowTypeWhenOverflow(BlobType type) {
+            return super.allowTypeWhenOverflow(type)
+                    || type == BlobType.MethodNonProfiled
+                    || type == BlobType.MethodProfiled;
+        }
+    },
     // Non-nmethods like Buffers, Adapters and Runtime Stubs
-    NonNMethod(2, "CodeHeap 'non-nmethods'", "NonNMethodCodeHeapSize") {
+    NonNMethod(3, "CodeHeap 'non-nmethods'", "NonNMethodCodeHeapSize") {
         @Override
         public boolean allowTypeWhenOverflow(BlobType type) {
             return super.allowTypeWhenOverflow(type)
@@ -56,7 +64,7 @@ public enum BlobType {
         }
     },
     // All types (No code cache segmentation)
-    All(3, "CodeCache", "ReservedCodeCacheSize");
+    All(4, "CodeCache", "ReservedCodeCacheSize");
 
     public final int id;
     public final String sizeOptionName;
@@ -98,6 +106,10 @@ public enum BlobType {
                 || whiteBox.getIntxVMFlag("TieredStopAtLevel") <= 1) {
             // there is no MethodProfiled in non tiered world or pure C1
             result.remove(MethodProfiled);
+        }
+
+        if (Long.valueOf(0).equals(whiteBox.getUintxVMFlag("HotCodeHeapSize"))) {
+            result.remove(MethodHot);
         }
         return result;
     }
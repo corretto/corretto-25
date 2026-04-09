405f3a893a5c07b7512fd2742c87bd890a90b637
Chad Rakoczy <crakoczy@openjdk.org>
2026-04-09 16:06:38 +0000

    8326205: Grouping frequently called C2 nmethods in CodeCache
    Co-authored-by: Evgeny Astigeevich <eastigeevich@openjdk.org>
    Reviewed-by: kvn, cslucas, aph

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1551,6 +1551,10 @@ const int ObjectAlignmentInBytes = 8;
           "Size of code heap with non-nmethods (in bytes)")                 \
           constraint(VMPageSizeConstraintFunc, AtParse)                     \
                                                                             \
+  product(uintx, HotCodeHeapSize, 0, EXPERIMENTAL,                          \
+          "Size of code heap with predicted hot methods (in bytes)")        \
+          range(0, max_uintx)                                               \
+                                                                            \
   product_pd(uintx, CodeCacheExpansionSize,                                 \
           "Code cache expansion size (in bytes)")                           \
           range(32*K, max_uintx)                                            \
